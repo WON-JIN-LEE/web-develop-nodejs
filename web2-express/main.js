ce6ca35fa094616673020acbcac6cea4d96a4ca3
WON-JIN-LEE <vopgh0409@gmail.com>
2021-11-27 17:29:15 +0900

    main.js 파일분리 by Express router

--- web2-express/main.js
+++ web2-express/main.js
@@ -2,16 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 var fs = require('fs');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
-var qs = require('querystring');
 var bodyParser = require('body-parser');
 var compression = require('compression');
+var topicRouter = require('./routes/topic');
+var indexRouter = require('./routes/index');
+var helmet = require('helmet')
 
+app.use(helmet());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
+
 app.get("*",function (request, response, next) {
   fs.readdir('./data', function (error, filelist) {
     request.list = filelist;
@@ -19,122 +20,10 @@ app.get("*",function (request, response, next) {
   });
 });
 
-//route ,routing
-app.get('/', (request, response) => {
-                var title = 'Welcome';
-                var description = "Hello, Node.js"
-                var list = template.list(request.list);
-                var html = template.HTML(title, list, `<h2>${title}</h2>
-    <p>${description}</p>    <img src="/images/laptop.jpg" style="width:300px; height:400px;">`,`<a href="/create">create</a>
-    `);
-                response.send(html);
+app.use('/', indexRouter);
+app.use('/topic', topicRouter);
 
-});
 
-//상세 페이지 구현
-app.get('/page/:pageId', (request, response,next) => {
-    var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    if (err) {
-        next(err);
-      } else {
-        
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
-      var list = template.list(request.list);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-                <a href="/update/${sanitizedTitle}">update</a>
-                <form action="/delete_process" method="post">
-                  <input type="hidden" name="id" value="${sanitizedTitle}">
-                  <input type="submit" value="delete">
-                </form>`
-      );
-      response.send(html);
-        }
-  });
-});
-
-//글 생성 데이터 입력
-app.get('/create', (request, response) => {
-          var title = 'WEB - create';
-          var list = template.list(request.list);
-          var html = template.HTML(title, list, `
-            <form action="/create_process" method="post">
-              <p><input type="text" name="title" placeholder="title"></p>
-              <p>
-                <textarea name="description" placeholder="description"></textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-          `, '');
-          response.send(html);
-});
-
-//글 생성 데이터 처리
-app.post('/create_process', (request, response) => {
-  var post = request.body
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-                 response.redirect(`/page/${title}`);
-    })
-});
-
-//글 수정 데이터 입력
-app.get('/update/:pageId', (request, response) => {
-        var filteredId = path.parse(request.params.pageId).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-          var title = request.params.pageId;
-          var list = template.list(request.list);
-          var html = template.HTML(title, list,
-            `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-          );
-          response.send(html);
-      });
-});
-
-//글 수정 데이터 처리
-app.post('/update_process', (request, response) => {
-          var post = request.body;
-          var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-              response.redirect(`/page/${title}`);
-
-            });
-          });
-});
-
-//글 삭제
-app.post('/delete_process', (request, response)  => {
-    var post = request.body;
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function (err) {
-      response.redirect('/');
-  });
-});
 
 app.use(function(req, res, next) {
   res.status(404).send('Sorry cant find that!');
